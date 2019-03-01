8c8be5a15d007b70ff3d9df7e231169e7b2b97e9
Adriana Lopez <alma.lopez@pulsarup.com>
2019-03-01 17:22:01 -0600

    stoybook installed

---
+++ src/components/modal/ModalBackground.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+
+class ModalBackground extends PureComponent {
+  render(){
+          return(
+              <div className="modal-background">
+                <p>Holi</p>
+              </div>
+          )
+      }
+  }
+
+
+export default ModalBackground;
