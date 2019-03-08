83110133947007d94b550e821de6f86b6f1ba4e1
Adriana Lopez <alma.lopez@pulsarup.com>
2019-03-08 10:55:33 -0600

    Index file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Modal components
+export Modal from "./components/modal/Modal.jsx";
+
+export ModalCard from "./components/modal/ModalCard.jsx"
+export ModalBackground from "./components/modal/ModalBackground.jsx";
+export ModalHeader from "./components/modal/ModalHeader.jsx";
+export ModalFooter from "./components/modal/ModalFooter.jsx";
+
+// Modal Medal
+export ModalMedal from "./components/modal/modalMedal/ModalMedal.jsx";
+export Medal from "./components/modal/modalMedal/Medal.jsx";
+
+// Buttons components
+export ButtonIcon from "./components/buttons/ButtonText.jsx";
+export ButtonText from "./components/buttons/ButtonIcon.jsx";
+
