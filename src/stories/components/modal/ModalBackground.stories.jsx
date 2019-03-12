1f9adcd1063b8df75b354852e897c4f008958bcf
Adriana Lopez <alma.lopez@pulsarup.com>
2019-03-12 12:03:06 -0600

    Fixed comments about Modal test

--- src/stories/components/modal/ModalBackground.stories.jsx
+++ src/stories/components/modal/ModalBackground.stories.jsx
@@ -5,5 +5,4 @@ import ModalBackground from "../../../components/modal/ModalBackground";
 storiesOf("ModalBackground", module)
   // Modal Background
   .add("Default", () => <ModalBackground show />)
-  .add("Red", () => <ModalBackground show color="red-d3" />)
-  .add("Purple", () => <ModalBackground show color="purple-d3" />);
+
