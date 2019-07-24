7fce5a688bbf1a25646f529e9f3ab4204d1f7d35
ady-pulsarup <alma.lopez@pulsarup.com>
2019-07-24 15:25:11 -0500

    fix: 🐛 Pulsarup icons added
    Pulsarup icons added

--- src/stories/components/modal/Icons.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Icon from "../../../components/icons/Icon";
-
-storiesOf("Icons", module)
-  // Icons
-  .add("Default", () => <Icon icon="phone" />);
